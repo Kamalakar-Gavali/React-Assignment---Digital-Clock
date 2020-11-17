8a16384e19fc76276a5600044ff6077ea14288ac
kamalakar <kg@gmail.com>
2020-11-17 12:37:50 +0530

    added date.toLocaleTimeString()

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,42 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    constructor(props)
-    {
+    constructor(props) {
         super(props);
-        this.state={time:new Date()};
+        this.state = { time: new Date() };
 
     }
     render() {
 
-        return(
+        return (
             <>
-               <div className="Clock">
-                   <h3 id="time">{this.getTimeString()}</h3>
-               </div>
+                <div className="Clock">
+                    <h3 id="time">{this.getTimeString()}</h3>
+                </div>
             </>
         )
     }
-    oneToTwoDigits(x)
-    {
-        return x<=9?`0${x}`:x; 
+    oneToTwoDigits(x) {
+        return x <= 9 ? `0${x}` : x;
     }
-    getTimeString()
-    {
-        const currrentHour=this.state.time.getHours();
-        const hourWith12format=currrentHour>12?currrentHour-12:currrentHour;
-        let AmOrPm =currrentHour>=12?'PM':'AM';
-        let currentMinutes=this.state.time.getMinutes();
-        let currentSec=this.state.time.getSeconds();
-   
-        return `${this.oneToTwoDigits(hourWith12format)}:${this.oneToTwoDigits(currentMinutes)}:${this.oneToTwoDigits(currentSec)} ${AmOrPm}`;
+    getTimeString() {
+        /*const currrentHour = this.state.time.getHours();
+        const hourWith12format = currrentHour > 12 ? currrentHour - 12 : currrentHour;
+        let AmOrPm = currrentHour >= 12 ? 'PM' : 'AM';
+        let currentMinutes = this.state.time.getMinutes();
+        let currentSec = this.state.time.getSeconds();
+
+        return `${this.oneToTwoDigits(hourWith12format)}:${this.oneToTwoDigits(currentMinutes)}:${this.oneToTwoDigits(currentSec)} ${AmOrPm}`;*/
+        return this.state.time.toLocaleTimeString();
 
     }
-    componentDidMount()
-    {
-        this.IntervalId=setInterval(()=>{
-            this.setState({time:new Date()});
-        },1000)
+    componentDidMount() {
+        this.IntervalId = setInterval(() => { 
+            this.setState({ time: new Date() }); 
+        }, 1000)
     }
-    componentWillUnmount()
-    {
+    componentWillUnmount() {
         clearInterval(this.IntervalId);
     }
 }
